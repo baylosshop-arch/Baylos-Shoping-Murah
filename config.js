2b0bbe96d22817cc91a0f3d678ced63498c02352
baylos <baylosshop@gmail.com>
2026-08-27 10:07:05 +0700

    Add configuration for Supabase connection

---
+++ config.js
@@ -0,0 +1,4 @@
+window.BAYLOS_CONFIG = {
+    SUPABASE_URL: "https://uuqlsrfpgyjfkhqmbtcg.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1cWxzcmZwZ3lqZmtocW1idGNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NzQyMjEsImV4cCI6MjEwMzM1MDIyMX0.qW-zocjT5I-KiLFYW0_tvgomA-KID-6kbJW7mPVxeRA"
+};

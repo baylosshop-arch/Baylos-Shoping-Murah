51a7985553a83fffb8e3d1db2a4e78f90279d28d
baylos <baylosshop@gmail.com>
2026-09-19 19:27:50 +0700

    Update Supabase URL and anon key in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BAYLOS_CONFIG = {
-    SUPABASE_URL: "https://uuqlsrfpgyjfkhqmbtcg.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1cWxzcmZwZ3lqZmtocW1idGNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NzQyMjEsImV4cCI6MjEwMzM1MDIyMX0.qW-zocjT5I-KiLFYW0_tvgomA-KID-6kbJW7mPVxeRA"
+    SUPABASE_URL: "https://xbvvabavlisiyxazfacs.supabase.co/rest/v1/",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhidnZhYmF2bGlzaXl4YXpmYWNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4MTI4NjYsImV4cCI6MjEwNTM4ODg2Nn0.o_6_UR8OD9xCm7tERBY_psl8fRhwQ4WZwiIQhwNnIDQ"
 };

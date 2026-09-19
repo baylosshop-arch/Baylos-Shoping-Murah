95d3ecc292d2a987e3aaf49901fa701e31696b30
baylos <baylosshop@gmail.com>
2026-09-19 21:01:25 +0700

    Update Supabase configuration URLs and keys

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BAYLOS_CONFIG = {
-    SUPABASE_URL: "https://xbvvabavlisiyxazfacs.supabase.co/rest/v1/",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhidnZhYmF2bGlzaXl4YXpmYWNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4MTI4NjYsImV4cCI6MjEwNTM4ODg2Nn0.o_6_UR8OD9xCm7tERBY_psl8fRhwQ4WZwiIQhwNnIDQ"
+    SUPABASE_URL: "https://emviceyluewhfwckuntj.supabase.co/rest/v1/",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtdmljZXlsdWV3aGZ3Y2t1bnRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4MjA1NjAsImV4cCI6MjEwNTM5NjU2MH0.FxSik1Yq8gYWdD6eMHrZmSsoxmQikOHPUmGiXgnfJBs"
 };

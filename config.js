24b08312604862458ef687ff218d3cbdac20f170
baylos <baylosshop@gmail.com>
2026-09-19 21:18:08 +0700

    Update SUPABASE_URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BAYLOS_CONFIG = {
-    SUPABASE_URL: "https://emviceyluewhfwckuntj.supabase.co/rest/v1/",
+    SUPABASE_URL: "https://emviceyluewhfwckuntj.supabase.co",
     SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtdmljZXlsdWV3aGZ3Y2t1bnRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4MjA1NjAsImV4cCI6MjEwNTM5NjU2MH0.FxSik1Yq8gYWdD6eMHrZmSsoxmQikOHPUmGiXgnfJBs"
 };
